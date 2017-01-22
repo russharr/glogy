02f0bf6aa62d2d99839d701c0bb9cf8c4ddd0206
russharr <rhdh@btinternet.com>
2017-01-22 16:17:58 +0000

    Hello world commit

---
+++ src/main/java/com/rhdh/App.java
@@ -0,0 +1,11 @@
+package com.rhdh;
+
+/**
+ * Created by Russell on 22/01/2017.
+ */
+public class App {
+
+    public static void main (String[] args) {
+        System.out.println("Hello World");
+    }
+}
